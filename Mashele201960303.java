70bc3f78590270d212a566a7cf4359a868704807
201960303 <105271786+201960303@users.noreply.github.com>
2022-06-15 12:23:15 +0200

    3.1.2

---
+++ Mashele201960303.java
@@ -0,0 +1,13 @@
+Public class Multiplier {
+
+
+public static void main(String[] args) {
+
+int a= 5;
+int b= 12;
+int c= a * b;
+
+System.out.println("The output for multiplication is :" + c );
+
+}
+}
